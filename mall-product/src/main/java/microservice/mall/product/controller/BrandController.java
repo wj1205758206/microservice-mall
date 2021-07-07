b4520ae4dac1166f3229fef4a6bff9c2ddb57a3b
weijian <1205758206@qq.com>
2021-07-07 15:00:18 +0800

    JSR303自定义校验注解

--- mall-product/src/main/java/microservice/mall/product/controller/BrandController.java
+++ mall-product/src/main/java/microservice/mall/product/controller/BrandController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import microservice.mall.common.valid.AddGroup;
 import microservice.mall.common.valid.UpdateGroup;
+import microservice.mall.common.valid.UpdateStatusGroup;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
@@ -88,6 +89,17 @@ public class BrandController {
     @RequestMapping("/update")
     //@RequiresPermissions("product:brand:update")
     public R update(@Validated(UpdateGroup.class) @RequestBody BrandEntity brand) {
+        brandService.updateById(brand);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改状态
+     */
+    @RequestMapping("/update/status")
+    //@RequiresPermissions("product:brand:update")
+    public R updateStatus(@Validated(UpdateStatusGroup.class) @RequestBody BrandEntity brand) {
         brandService.updateById(brand);
 
         return R.ok();
--- mall-product/src/main/java/microservice/mall/product/entity/BrandEntity.java
+++ mall-product/src/main/java/microservice/mall/product/entity/BrandEntity.java
@@ -8,7 +8,9 @@ import java.util.Date;
 
 import lombok.Data;
 import microservice.mall.common.valid.AddGroup;
+import microservice.mall.common.valid.ListValue;
 import microservice.mall.common.valid.UpdateGroup;
+import microservice.mall.common.valid.UpdateStatusGroup;
 import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.*;
@@ -29,7 +31,7 @@ public class BrandEntity implements Serializable {
      * 品牌id
      */
     @NotNull(message = "修改必须指定品牌id", groups = {UpdateGroup.class})
-    @Null(message = "新增不能指定id", groups = {AddGroup.class, UpdateGroup.class})
+    @Null(message = "新增不能指定id", groups = {AddGroup.class})
     @TableId
     private Long brandId;
     /**
@@ -50,12 +52,14 @@ public class BrandEntity implements Serializable {
     /**
      * 显示状态[0-不显示；1-显示]
      */
+    @NotNull(groups = {AddGroup.class, UpdateStatusGroup.class})
+    @ListValue(vals = {0, 1}, groups = {AddGroup.class, UpdateStatusGroup.class})
     private Integer showStatus;
     /**
      * 检索首字母
      */
     @NotEmpty(groups = {AddGroup.class})
-    @Pattern(regexp = "/^[a-zA-z]$/", message = "检索首字母必须是a-z或者A-Z", groups = {AddGroup.class, UpdateGroup.class})
+    @Pattern(regexp = "^[a-zA-z]$", message = "检索首字母必须是a-z或者A-Z", groups = {AddGroup.class, UpdateGroup.class})
     private String firstLetter;
     /**
      * 排序
