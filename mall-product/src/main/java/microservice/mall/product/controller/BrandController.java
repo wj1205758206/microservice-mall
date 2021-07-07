99ec652e39a6f1544af973a1fc56d52b593df3ac
weijian <1205758206@qq.com>
2021-07-07 10:35:34 +0800

    使用JSR303注解数据校验

--- mall-product/src/main/java/microservice/mall/product/controller/BrandController.java
+++ mall-product/src/main/java/microservice/mall/product/controller/BrandController.java
@@ -1,10 +1,12 @@
 package microservice.mall.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import microservice.mall.product.service.BrandService;
 import microservice.mall.common.utils.PageUtils;
 import microservice.mall.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -36,7 +39,7 @@ public class BrandController {
      */
     @RequestMapping("/list")
     @RequiresPermissions("product:brand:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = brandService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -48,8 +51,8 @@ public class BrandController {
      */
     @RequestMapping("/info/{brandId}")
     @RequiresPermissions("product:brand:info")
-    public R info(@PathVariable("brandId") Long brandId){
-		BrandEntity brand = brandService.getById(brandId);
+    public R info(@PathVariable("brandId") Long brandId) {
+        BrandEntity brand = brandService.getById(brandId);
 
         return R.ok().put("brand", brand);
     }
@@ -59,8 +62,20 @@ public class BrandController {
      */
     @RequestMapping("/save")
     @RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
+    public R save(@Valid @RequestBody BrandEntity brand, BindingResult bindingResult) {
+        Map<String, String> map = new HashMap<>();
+        if (bindingResult.hasErrors()) {
+            bindingResult.getFieldErrors().forEach((item) -> {
+                String field = item.getField();
+                String message = item.getDefaultMessage();
+                map.put(field, message);
+            });
+            return R.error(400, "提交的数据不合法!").put("data", map);
+
+        } else {
+            brandService.save(brand);
+        }
+
 
         return R.ok();
     }
@@ -70,8 +85,8 @@ public class BrandController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("product:brand:update")
-    public R update(@RequestBody BrandEntity brand){
-		brandService.updateById(brand);
+    public R update(@RequestBody BrandEntity brand) {
+        brandService.updateById(brand);
 
         return R.ok();
     }
@@ -81,8 +96,8 @@ public class BrandController {
      */
     @RequestMapping("/delete")
     @RequiresPermissions("product:brand:delete")
-    public R delete(@RequestBody Long[] brandIds){
-		brandService.removeByIds(Arrays.asList(brandIds));
+    public R delete(@RequestBody Long[] brandIds) {
+        brandService.removeByIds(Arrays.asList(brandIds));
 
         return R.ok();
     }
--- mall-product/src/main/java/microservice/mall/product/entity/BrandEntity.java
+++ mall-product/src/main/java/microservice/mall/product/entity/BrandEntity.java
@@ -5,11 +5,15 @@ import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.*;
 
 /**
  * 品牌
- * 
+ *
  * @author wjian
  * @email wjian@gmail.com
  * @date 2021-07-04 09:48:57
@@ -17,36 +21,43 @@ import lombok.Data;
 @Data
 @TableName("pms_brand")
 public class BrandEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 品牌id
-	 */
-	@TableId
-	private Long brandId;
-	/**
-	 * 品牌名
-	 */
-	private String name;
-	/**
-	 * 品牌logo地址
-	 */
-	private String logo;
-	/**
-	 * 介绍
-	 */
-	private String descript;
-	/**
-	 * 显示状态[0-不显示；1-显示]
-	 */
-	private Integer showStatus;
-	/**
-	 * 检索首字母
-	 */
-	private String firstLetter;
-	/**
-	 * 排序
-	 */
-	private Integer sort;
+    /**
+     * 品牌id
+     */
+    @TableId
+    private Long brandId;
+    /**
+     * 品牌名
+     */
+    @NotBlank
+    private String name;
+    /**
+     * 品牌logo地址
+     */
+    @NotEmpty
+    @URL(message = "logo必须是一个合法的url地址")
+    private String logo;
+    /**
+     * 介绍
+     */
+    private String descript;
+    /**
+     * 显示状态[0-不显示；1-显示]
+     */
+    private Integer showStatus;
+    /**
+     * 检索首字母
+     */
+    @NotEmpty
+    @Pattern(regexp = "/^[a-zA-z]$/", message = "检索首字母必须是a-z或者A-Z")
+    private String firstLetter;
+    /**
+     * 排序
+     */
+    @NotNull
+    @Min(value = 0, message = "排序必须是一个大于等于0的整数")
+    private Integer sort;
 
 }
