efec3bb5702b4620156112c5f518d06fb49ae70b
weijian <1205758206@qq.com>
2021-07-13 10:53:55 +0800

    添加查询库存功能

--- mall-ware/src/main/java/microservice/mall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ mall-ware/src/main/java/microservice/mall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,7 +1,9 @@
 package microservice.mall.ware.service.impl;
 
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +13,7 @@ import microservice.mall.common.utils.Query;
 import microservice.mall.ware.dao.PurchaseDetailDao;
 import microservice.mall.ware.entity.PurchaseDetailEntity;
 import microservice.mall.ware.service.PurchaseDetailService;
+import org.springframework.util.StringUtils;
 
 
 @Service("purchaseDetailService")
@@ -18,9 +21,26 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<PurchaseDetailEntity> purchaseDetailEntityQueryWrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            purchaseDetailEntityQueryWrapper.and((wrapper) -> {
+                wrapper.eq("id", key).or().eq("purchase_id", key).or().eq("sku_id", key);
+            });
+        }
+        String status = (String) params.get("status");
+        if (!StringUtils.isEmpty(status)) {
+            purchaseDetailEntityQueryWrapper.eq("status", status);
+        }
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(wareId)) {
+            purchaseDetailEntityQueryWrapper.eq("ware_id", wareId);
+        }
+
         IPage<PurchaseDetailEntity> page = this.page(
                 new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
+                purchaseDetailEntityQueryWrapper
         );
 
         return new PageUtils(page);
