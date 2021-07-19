56d8de37f4ff5c9594d1a54866b0a2d8fa2a668b
weijian <1205758206@qq.com>
2021-07-19 16:15:37 +0800

    整合thymeleaf渲染首页

---
+++ mall-product/src/main/java/microservice/mall/product/app/AttrGroupController.java
@@ -0,0 +1,147 @@
+package microservice.mall.product.app;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import microservice.mall.product.entity.AttrEntity;
+import microservice.mall.product.service.AttrAttrgroupRelationService;
+import microservice.mall.product.service.AttrService;
+import microservice.mall.product.service.CategoryService;
+import microservice.mall.product.vo.AttrGroupRelationVo;
+import microservice.mall.product.vo.AttrGroupWithAttrsVo;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import microservice.mall.product.entity.AttrGroupEntity;
+import microservice.mall.product.service.AttrGroupService;
+import microservice.mall.common.utils.PageUtils;
+import microservice.mall.common.utils.R;
+
+
+/**
+ * 属性分组
+ *
+ * @author wjian
+ * @email wjian@gmail.com
+ * @date 2021-07-04 09:48:57
+ */
+@RestController
+@RequestMapping("product/attrgroup")
+public class AttrGroupController {
+    @Autowired
+    private AttrGroupService attrGroupService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private AttrService attrService;
+
+    @Autowired
+    private AttrAttrgroupRelationService attrAttrgroupRelationService;
+
+    /**
+     * 获取分类下所有的分组以及属性
+     */
+    @GetMapping("/{catelogId}/withattr")
+    public R getAttrGroupWithAttr(@PathVariable("catelogId") Long catelogId) {
+
+        List<AttrGroupWithAttrsVo> vos = attrGroupService.getAttrGroupWithAttrByCatelogId(catelogId);
+
+        return R.ok().put("data", vos);
+    }
+
+    /**
+     * 查询属性分组未关联的属性
+     */
+    @GetMapping("/{attrgroupId}/noattr/relation")
+    public R attrNoRelationList(@RequestParam Map<String, Object> params,
+                                @PathVariable("attrgroupId") Long attrgroupId) {
+
+        PageUtils page = attrService.getNoRelationAttr(params, attrgroupId);
+        return R.ok().put("page", page);
+    }
+
+    /**
+     * 移除属性分组关联的属性
+     */
+    @PostMapping("/attr/relation/delete")
+    public R deleteRelation(@RequestBody AttrGroupRelationVo[] vos) {
+        attrService.deleteRelation(vos);
+        return R.ok();
+    }
+
+    /**
+     * 获取属性分组关联的所有属性
+     */
+    @GetMapping("/{attrgroupId}/attr/relation")
+    public R attrRelationList(@RequestParam Map<String, Object> params,
+                              @PathVariable("attrgroupId") Long attrgroupId) {
+
+        List<AttrEntity> attrEntityList = attrService.getRelationAttr(attrgroupId);
+        return R.ok().put("data", attrEntityList);
+    }
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list/{categoryId}")
+    //@RequiresPermissions("product:attrgroup:list")
+    public R list(@RequestParam Map<String, Object> params,
+                  @PathVariable("categoryId") Long catelogId) {
+        //PageUtils page = attrGroupService.queryPage(params);
+        PageUtils page = attrGroupService.queryPage(params, catelogId);
+        return R.ok().put("page", page);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{attrGroupId}")
+    @RequiresPermissions("product:attrgroup:info")
+    public R info(@PathVariable("attrGroupId") Long attrGroupId) {
+        AttrGroupEntity attrGroup = attrGroupService.getById(attrGroupId);
+
+        Long[] path = categoryService.findCatelogPath(attrGroup.getCatelogId());
+        attrGroup.setCatelogPath(path);
+
+        return R.ok().put("attrGroup", attrGroup);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    @RequiresPermissions("product:attrgroup:save")
+    public R save(@RequestBody AttrGroupEntity attrGroup) {
+        attrGroupService.save(attrGroup);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    @RequiresPermissions("product:attrgroup:update")
+    public R update(@RequestBody AttrGroupEntity attrGroup) {
+        attrGroupService.updateById(attrGroup);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    @RequiresPermissions("product:attrgroup:delete")
+    public R delete(@RequestBody Long[] attrGroupIds) {
+        attrGroupService.removeByIds(Arrays.asList(attrGroupIds));
+
+        return R.ok();
+    }
+
+}
---
+++ mall-product/src/main/java/microservice/mall/product/app/CategoryBrandRelationController.java
@@ -0,0 +1,125 @@
+package microservice.mall.product.app;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import microservice.mall.product.entity.BrandEntity;
+import microservice.mall.product.vo.BrandVo;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import microservice.mall.product.entity.CategoryBrandRelationEntity;
+import microservice.mall.product.service.CategoryBrandRelationService;
+import microservice.mall.common.utils.PageUtils;
+import microservice.mall.common.utils.R;
+
+
+/**
+ * 品牌分类关联
+ *
+ * @author wjian
+ * @email wjian@gmail.com
+ * @date 2021-07-04 09:48:57
+ */
+@RestController
+@RequestMapping("product/categorybrandrelation")
+public class CategoryBrandRelationController {
+    @Autowired
+    private CategoryBrandRelationService categoryBrandRelationService;
+
+    /**
+     * 获取三级分类关联的品牌列表
+     */
+    @GetMapping("/brands/list")
+    //@RequiresPermissions("product:categorybrandrelation:list")
+    public R getCategoryBrand(@RequestParam(value = "catId", required = true) Long catId) {
+        List<BrandEntity> brandVos = categoryBrandRelationService.getBrandByCatId(catId);
+
+        List<BrandVo> collect = brandVos.stream().map((item) -> {
+            BrandVo brandVo = new BrandVo();
+            brandVo.setBrandId(item.getBrandId());
+            brandVo.setBrandName(item.getName());
+            return brandVo;
+        }).collect(Collectors.toList());
+
+        return R.ok().put("data", collect);
+    }
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    @RequiresPermissions("product:categorybrandrelation:list")
+    public R list(@RequestParam Map<String, Object> params) {
+        PageUtils page = categoryBrandRelationService.queryPage(params);
+
+        return R.ok().put("page", page);
+    }
+
+    /**
+     * 查询指定品牌id的所属分类
+     *
+     * @param brandId 品牌id
+     * @return 品牌id所属分类的集合
+     */
+    @GetMapping("/catelog/list")
+    //@RequiresPermissions("product:categorybrandrelation:list")
+    public R catelogList(@RequestParam("brandId") Long brandId) {
+
+        QueryWrapper<CategoryBrandRelationEntity> categoryBrandRelationEntityQueryWrapper = new QueryWrapper<>();
+        categoryBrandRelationEntityQueryWrapper.eq("brand_id", brandId);
+        List<CategoryBrandRelationEntity> data =
+                categoryBrandRelationService.list(categoryBrandRelationEntityQueryWrapper);
+
+        return R.ok().put("data", data);
+    }
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{id}")
+    @RequiresPermissions("product:categorybrandrelation:info")
+    public R info(@PathVariable("id") Long id) {
+        CategoryBrandRelationEntity categoryBrandRelation = categoryBrandRelationService.getById(id);
+
+        return R.ok().put("categoryBrandRelation", categoryBrandRelation);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    //@RequiresPermissions("product:categorybrandrelation:save")
+    public R save(@RequestBody CategoryBrandRelationEntity categoryBrandRelation) {
+        categoryBrandRelationService.saveDetail(categoryBrandRelation);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    @RequiresPermissions("product:categorybrandrelation:update")
+    public R update(@RequestBody CategoryBrandRelationEntity categoryBrandRelation) {
+        categoryBrandRelationService.updateById(categoryBrandRelation);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    @RequiresPermissions("product:categorybrandrelation:delete")
+    public R delete(@RequestBody Long[] ids) {
+        categoryBrandRelationService.removeByIds(Arrays.asList(ids));
+
+        return R.ok();
+    }
+
+}
